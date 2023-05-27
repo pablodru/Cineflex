98d34f24dff08eed6fa7621bf0fe89bec3e87bbf
pablodru <pdrocha03@gmail.com>
2023-05-27 18:46:28 -0300

    alteração no botao de voltar page

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -5,6 +5,6 @@ export default function Back(){
     const navigate = useNavigate();
 
     return (
-        <ion-icon onClick={() => navigate(-1)} name="arrow-back-outline" style={{width:'30px', height:'30px', color:'#000000', position:'fixed', top:'23px', left:'10px'}}></ion-icon>
+        <button data-test='go-home-header-btn' onClick={() => navigate(-1)}><ion-icon name="arrow-back-outline" style={{width:'30px', height:'30px', color:'#000000', position:'fixed', top:'23px', left:'10px'}} ></ion-icon></button>
     ) 
 }
